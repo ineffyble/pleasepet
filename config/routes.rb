cb73abd2d97e1c0bb743c236902494932f852294
Effy Elden <git@effy.is>
2017-02-18 15:33:18 +1100

    Make adaptive text work on turbolink refresh, underline links, manual Evelyn route

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,13 @@
 Rails.application.routes.draw do
+  root 'index#view'
+  devise_for :pets, controllers: { registrations: 'registrations' }
+
+  get '/evelyn' => redirect("http://evelyn.pet")
+
   resources :noises
   resources :pettings
-  root 'index#view'
-
   resources :pages, path: '', only: [:show, :update], param: :url
   resources :pages, param: :url
   post '/:url/pet' => 'pages#pet'
-  devise_for :pets, controllers: { registrations: 'registrations' }
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
