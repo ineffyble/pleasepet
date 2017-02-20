a097fa5b7c9493ae596310f81d73ca9ce89ae4a4
Effy Elden <git@effy.is>
2017-02-20 20:56:07 +1100

    Allow underscore in page URLs

--- app/models/page.rb
+++ app/models/page.rb
@@ -1,5 +1,5 @@
 class Page < ApplicationRecord
-  VALID_URL_REGEX = /\A[\-a-zA-Z0-9]+\z/
+  VALID_URL_REGEX = /\A[\_\-a-zA-Z0-9]+\z/
   validates :url, presence: true, uniqueness: { case_sensitive: false }, format: { with: VALID_URL_REGEX }
   belongs_to :pet, inverse_of: :page
   has_many :noises, inverse_of: :page
