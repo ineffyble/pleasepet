7e390cc01a9d6c4e4f6556da377246a407ecb02a
Effy Elden <git@effy.is>
2017-03-01 09:31:09 +0000

    Allow noises to be WAV files

--- app/models/noise.rb
+++ app/models/noise.rb
@@ -3,5 +3,5 @@ class Noise < ApplicationRecord
 
   has_attached_file :sound
   validates_attachment_content_type :sound,
-                                    :content_type => [ 'audio/mpeg', 'audio/x-mpeg', 'audio/mp3', 'audio/x-mp3', 'audio/mpeg3', 'audio/x-mpeg3', 'audio/mpg', 'audio/x-mpg', 'audio/x-mpegaudio' ]
+                                    :content_type => [ 'audio/mpeg', 'audio/x-mpeg', 'audio/mp3', 'audio/x-mp3', 'audio/mpeg3', 'audio/x-mpeg3', 'audio/mpg', 'audio/x-mpg', 'audio/x-mpegaudio', 'audio/wave', 'audio/x-wav', 'audio/wav' ]
 end
